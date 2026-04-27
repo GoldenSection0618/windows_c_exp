2ae487a692d1e62e0e16849fd4fb5a46a9b8d6f0
Yeung Lihan <yanson0557@gmail.com>
2026-04-27 13:59:12 +0800

    refactor: table-drive GUI navigation routes

--- C_PROGRAM_EXPERIMENT/src/presentation/gui/gui_main_window_core.cpp
+++ C_PROGRAM_EXPERIMENT/src/presentation/gui/gui_main_window_core.cpp
@@ -1,4 +1,4 @@
-﻿#include "gui_main_window_core.h"
+#include "gui_main_window_core.h"
 
 #include "gui_main_window_internal.h"
 
@@ -8,8 +8,93 @@
 #include <commctrl.h>
 #include <windows.h>
 
-
 #pragma comment(lib, "Comctl32.lib")
+
+enum class NavAction {
+    SwitchMode,
+    Logout,
+    Exit
+};
+
+struct NavRoute {
+    LoginRole role;
+    int controlId;
+    NavAction action;
+    GuiMode targetMode;
+};
+
+static const NavRoute kNavRoutes[] = {
+    {LOGIN_ROLE_NONE, IDC_AMS_NAV_ADD_CARD, NavAction::SwitchMode, GuiMode::AuthAdmin},
+    {LOGIN_ROLE_NONE, IDC_AMS_NAV_QUERY_CARD, NavAction::SwitchMode, GuiMode::AuthUserLogin},
+    {LOGIN_ROLE_NONE, IDC_AMS_NAV_LOGON, NavAction::SwitchMode, GuiMode::AuthRegister},
+    {LOGIN_ROLE_NONE, IDC_AMS_NAV_SETTLE, NavAction::Exit, GuiMode::AuthAdmin},
+
+    {LOGIN_ROLE_ADMIN, IDC_AMS_NAV_ADD_CARD, NavAction::SwitchMode, GuiMode::AdminQuery},
+    {LOGIN_ROLE_ADMIN, IDC_AMS_NAV_QUERY_CARD, NavAction::SwitchMode, GuiMode::AdminStop},
+    {LOGIN_ROLE_ADMIN, IDC_AMS_NAV_LOGON, NavAction::SwitchMode, GuiMode::AdminRecharge},
+    {LOGIN_ROLE_ADMIN, IDC_AMS_NAV_SETTLE, NavAction::SwitchMode, GuiMode::AdminRefund},
+    {LOGIN_ROLE_ADMIN, IDC_AMS_NAV_RECHARGE, NavAction::SwitchMode, GuiMode::AdminStatistics},
+    {LOGIN_ROLE_ADMIN, IDC_AMS_NAV_REFUND, NavAction::Logout, GuiMode::AuthAdmin},
+
+    {LOGIN_ROLE_USER, IDC_AMS_NAV_ADD_CARD, NavAction::SwitchMode, GuiMode::UserBalance},
+    {LOGIN_ROLE_USER, IDC_AMS_NAV_QUERY_CARD, NavAction::SwitchMode, GuiMode::UserStart},
+    {LOGIN_ROLE_USER, IDC_AMS_NAV_LOGON, NavAction::SwitchMode, GuiMode::UserStop},
+    {LOGIN_ROLE_USER, IDC_AMS_NAV_SETTLE, NavAction::SwitchMode, GuiMode::UserRecharge},
+    {LOGIN_ROLE_USER, IDC_AMS_NAV_RECHARGE, NavAction::SwitchMode, GuiMode::UserRefund},
+    {LOGIN_ROLE_USER, IDC_AMS_NAV_REFUND, NavAction::SwitchMode, GuiMode::UserCancel},
+    {LOGIN_ROLE_USER, IDC_AMS_NAV_CANCEL_CARD, NavAction::Logout, GuiMode::AuthAdmin},
+
+    {LOGIN_ROLE_NONE, IDC_AMS_NAV_EXIT, NavAction::Exit, GuiMode::AuthAdmin},
+    {LOGIN_ROLE_ADMIN, IDC_AMS_NAV_EXIT, NavAction::Exit, GuiMode::AuthAdmin},
+    {LOGIN_ROLE_USER, IDC_AMS_NAV_EXIT, NavAction::Exit, GuiMode::AuthAdmin},
+};
+
+static LoginRole CurrentRole(const GuiState *state)
+{
+    if (state == nullptr) {
+        return LOGIN_ROLE_NONE;
+    }
+    if (bizIsAdminSession(&state->session)) {
+        return LOGIN_ROLE_ADMIN;
+    }
+    if (bizIsUserSession(&state->session)) {
+        return LOGIN_ROLE_USER;
+    }
+    return LOGIN_ROLE_NONE;
+}
+
+static const NavRoute *FindNavRoute(LoginRole role, int controlId)
+{
+    for (const NavRoute &route : kNavRoutes) {
+        if (route.role == role && route.controlId == controlId) {
+            return &route;
+        }
+    }
+    return nullptr;
+}
+
+static int HandleNavCommand(HWND dialog, GuiState *state, int controlId)
+{
+    const NavRoute *route = FindNavRoute(CurrentRole(state), controlId);
+    if (route == nullptr) {
+        return 0;
+    }
+
+    switch (route->action) {
+    case NavAction::SwitchMode:
+        GuiSwitchMode(dialog, state, route->targetMode);
+        return 1;
+    case NavAction::Logout:
+        GuiLogoutToLogin(dialog, state);
+        return 1;
+    case NavAction::Exit:
+        EndDialog(dialog, 0);
+        return 1;
+    default:
+        return 0;
+    }
+}
+
 static void ResizeAndCenterDialog(HWND dialog)
 {
     int clientWidth = 760;
@@ -47,71 +132,12 @@ static INT_PTR CALLBACK MainDialogProc(HWND dialog, UINT message, WPARAM wParam,
             return FALSE;
         }
 
-        switch (LOWORD(wParam)) {
-        case IDC_AMS_SUBMIT:
+        if (LOWORD(wParam) == IDC_AMS_SUBMIT) {
             GuiExecuteSubmit(dialog, state);
             return TRUE;
-        case IDC_AMS_NAV_ADD_CARD:
-            if (bizIsAdminSession(&state->session)) {
-                GuiSwitchMode(dialog, state, GuiMode::AdminQuery);
-            } else if (bizIsUserSession(&state->session)) {
-                GuiSwitchMode(dialog, state, GuiMode::UserBalance);
-            } else {
-                GuiSwitchMode(dialog, state, GuiMode::AuthAdmin);
-            }
-            return TRUE;
-        case IDC_AMS_NAV_QUERY_CARD:
-            if (bizIsAdminSession(&state->session)) {
-                GuiSwitchMode(dialog, state, GuiMode::AdminStop);
-            } else if (bizIsUserSession(&state->session)) {
-                GuiSwitchMode(dialog, state, GuiMode::UserStart);
-            } else {
-                GuiSwitchMode(dialog, state, GuiMode::AuthUserLogin);
-            }
-            return TRUE;
-        case IDC_AMS_NAV_LOGON:
-            if (bizIsAdminSession(&state->session)) {
-                GuiSwitchMode(dialog, state, GuiMode::AdminRecharge);
-            } else if (bizIsUserSession(&state->session)) {
-                GuiSwitchMode(dialog, state, GuiMode::UserStop);
-            } else {
-                GuiSwitchMode(dialog, state, GuiMode::AuthRegister);
-            }
-            return TRUE;
-        case IDC_AMS_NAV_SETTLE:
-            if (bizIsAdminSession(&state->session)) {
-                GuiSwitchMode(dialog, state, GuiMode::AdminRefund);
-            } else if (bizIsUserSession(&state->session)) {
-                GuiSwitchMode(dialog, state, GuiMode::UserRecharge);
-            } else {
-                EndDialog(dialog, 0);
-            }
-            return TRUE;
-        case IDC_AMS_NAV_RECHARGE:
-            if (bizIsAdminSession(&state->session)) {
-                GuiSwitchMode(dialog, state, GuiMode::AdminStatistics);
-            } else if (bizIsUserSession(&state->session)) {
-                GuiSwitchMode(dialog, state, GuiMode::UserRefund);
-            }
-            return TRUE;
-        case IDC_AMS_NAV_REFUND:
-            if (bizIsAdminSession(&state->session)) {
-                GuiLogoutToLogin(dialog, state);
-            } else if (bizIsUserSession(&state->session)) {
-                GuiSwitchMode(dialog, state, GuiMode::UserCancel);
-            }
-            return TRUE;
-        case IDC_AMS_NAV_CANCEL_CARD:
-            if (bizIsUserSession(&state->session)) {
-                GuiLogoutToLogin(dialog, state);
-            }
-            return TRUE;
-        case IDC_AMS_NAV_EXIT:
-            EndDialog(dialog, 0);
-            return TRUE;
-        default:
-            return FALSE;
         }
+
+        return HandleNavCommand(dialog, state, LOWORD(wParam)) ? TRUE : FALSE;
     case WM_CLOSE:
         EndDialog(dialog, 0);
         return TRUE;
