060f03fe387b15bbcd84e39f4c4785518d6c88c7
Yeung Lihan <yanson0557@gmail.com>
2026-04-27 15:26:44 +0800

    Declare advanced card query view

--- C_PROGRAM_EXPERIMENT/include/card_view.h
+++ C_PROGRAM_EXPERIMENT/include/card_view.h
@@ -2,12 +2,19 @@
 #define CARD_VIEW_H
 
 #include <stddef.h>
+#include <stdint.h>
 
+#include "card_query.h"
 #include "model.h"
 
 void viewShowCardSummary(const Card *card);
 void viewShowQueryCardDetails(const Card *card);
 void viewShowFuzzyQueryResults(const char *keyword, const Card *cards, size_t count);
+void viewShowAdvancedQueryPage(const Card *cards,
+                               size_t count,
+                               size_t pageIndex,
+                               size_t pageSize,
+                               const CardQueryOption *option);
 void viewShowLogonInfo(const LogonInfo *logonInfo);
 void viewShowSettleInfo(const SettleInfo *settleInfo);
 void viewShowRechargeInfo(const Card *card, int32_t rechargeAmountCent);
